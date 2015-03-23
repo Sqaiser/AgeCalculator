8b16d8031b5ca438af366ec0d9d6a81f4cd8b493
15063802 <15063802@CST_N417_22.cst.citytech.cuny.edu>
2015-03-23 17:21:08 -0400

    I did all of the extra credit work.

--- AgeCalculator/src/cst1201/AgeCalculator.java
+++ AgeCalculator/src/cst1201/AgeCalculator.java
@@ -6,39 +6,38 @@ import java.util.Scanner;
 /**
  * Calculates ages using a given year.
  *
- * @author Raffi Khatchadourian.
+ * @author Sameen.Qaiser
  */
 public class AgeCalculator {
 
 	public static void main(String[] args) {
 		//Get the current year.
-		int currentYear = LocalDate.now().getYear();
+              int currentYear = LocalDate.now().getYear();
+               //Ask the user for their age.
+              System.out.print("How old are you? ");
+              //Create a scanner to retrieve user input.
+            Scanner scanner = new Scanner(System.in);
+              //Retrieve user input.
+            int currentAge = scanner.nextInt();
+             //Ask the user for another year.
+           System.out.print("Enter another year: ");
+           int anotherYear = scanner.nextInt();
+         //Calculate the user's age in the year retrieved in the previous step.
+          //First, we need to know how many years to add to the user's current age.
+          //This will be the other year minus the current year.
+          int yearsToAdd = anotherYear - currentYear;
+          //Next, we add the result from above to the user's current age.
+         int newAge = currentAge + yearsToAdd;
+           //Finally, display the user's age in this year, but only if it's valid.
+          if (newAge > currentAge){
+          System.out.println("You will be " + newAge + "-years-old in "+ anotherYear + ".");}
+         else if (newAge <0 )
+        System.out.println("You weren't born ");
+        else if (newAge <= 1)
+        System.out.println("You were " + newAge + "-year-old in "+ anotherYear + ".");
+        else if (newAge < currentAge)
+        System.out.println("You were " + newAge + "-years-old in "+ anotherYear + ".");
 
-		//Ask the user for their age.
-		System.out.print("How old are you? ");
-
-		//Create a scanner to retrieve user input.
-		Scanner scanner = new Scanner(System.in);
-
-		//Retrieve user input.
-		int currentAge = scanner.nextInt();
-
-		//Ask the user for another year.
-		System.out.print("Enter another year: ");
-		int anotherYear = scanner.nextInt();
-
-		//Calculate the user's age in the year retrieved in the previous step.
-		//First, we need to know how many years to add to the user's current age.
-		//This will be the other year minus the current year.
-		int yearsToAdd = anotherYear - currentYear;
-
-		//Next, we add the result from above to the user's current age.
-		int newAge = currentAge + yearsToAdd;
-
-		//Finally, display the user's age in this year, but only if it's valid.
-		if (newAge >= 0)
-			System.out.println("You will be " + newAge + "-years-old in "
-				+ anotherYear + ".");
 
 		/* 
 		 EXTRA CREDIT 1: Use an else statement above to output an error message. This
